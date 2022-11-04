b121754c4e497316a81d320348353ccd793b51e5
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-11-04 16:13:21 +0530

    feat : add data and event handling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
+// import { render } from '@testing-library/react';
+import React, {Component} from 'react';
 import './App.css';
-import Navbar from './Components/Navbar'
-import TextForm from './Components/TextForm';
-function App() {
-  return (
-    <>
-    <Navbar title='TextUtils'/>
-    <div className='container my-3'>
-    <TextForm heading = 'Enter your text to analyze below'/>
-    </div>
-    </>
+import Employee from './Components/Employee';
 
 
+class App extends Component {
+  state={employees:[
+    {
+      "key":1,
+      "name": "ABC",
+      "date_of_birth": "10/10/2010",
+      "position_held": "Manager"
+    },
+    {
+      "key":2,
+      "name": "John",
+      "date_of_birth": "10/10/2010",
+      "position_held": "Manager"
+    },
+    {
+      "key":3,
+      "name": "Michael",
+      "date_of_birth": "10/10/2010",
+      "position_held": "Manager"
+    },
+    {
+      "key":4,
+      "name": "Michael",
+      "date_of_birth": "10/10/2010",
+      "position_held": "Manager"
+    },
+    {
+      "key":5,
+      "name": "Michael",
+      "date_of_birth": "10/10/2010",
+      "position_held": "Manager"
+    },
+    {
+      "key":6,
+      "name": "Michael",
+      "date_of_birth": "10/10/2010",
+      "position_held": "Manager"
+    }
+  ]
+}
+
+ 
+
+componentDidMount() {
+
+}
    
-)}
+render() {
+  return (<div>
+    {
+      this.state.employees.map((emp) => {
+      return <Employee name={emp.name} dob={emp.date_of_birth} pos={emp.position_held} onClickHandler={(event)=>{
+      console.log("click event", Event);
+      this.setState({employees:[]});
+    }} /> }
+  )}
+  </div>
 
+  )
+}
 
+}
 export default App;
