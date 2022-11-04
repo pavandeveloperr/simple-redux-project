b7d46714b06db7be5345bc198061045f0854c76c
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-11-04 16:13:38 +0530

    refactor

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./App.css"
+import "../App.css"
 
-const Employee = () => {
+const Employee = (props) => {
     return (
         <div className="employeeStyle">
-            <div><label>Employee Name : </label><input type="text" value="ABC"></input></div>
-            <div><label>DOB : </label><input type="text" value="10/10/2010"></input></div>
-            <div><label>Position Held : </label><input type="text" value="Manager"></input></div>
+            <div><label>Employee Name : </label><input type="text" value={props.name} onClick={props.onClickHandler}></input></div>
+            <div><label>DOB : </label><input type="text" value={props.dob}></input></div>
+            <div><label>Position Held : </label><input type="text" value={props.pos}></input></div>
         </div>
     )
 }
